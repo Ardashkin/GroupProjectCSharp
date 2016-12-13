bea93cb01f6b60f26a8b13b0d12b183b81a92bbf
Gregory Golovko <golovko.greg@gmail.com>
2016-12-13 03:32:57 +0200

    user model has implemented

---
+++ CSharpShop/DomainModel/BaseModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Collections;
+
+namespace DomainModel
+{
+    [DataContract]
+    public abstract class BaseModel : INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+        public bool HasErrors
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [DataMember]
+        [Key]
+        public Guid Id { get; set; }
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ CSharpShop/DomainModel/User.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+
+namespace DomainModel
+{
+    [DataContract]
+    public class User : BaseModel
+    {
+        private string firstName;
+        private string lastName;
+        private string patronimic;
+        private string login;
+        private string password;
+        private string phone;
+        private string address;
+        private UserType userType;
+        [DataMember]
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged(nameof(this.FirstName));
+            }
+        }
+        [DataMember]
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged(nameof(this.LastName));
+            }
+        }
+        [DataMember]
+        public string Patronimic
+        {
+            get { return patronimic; }
+            set
+            {
+                patronimic = value;
+                OnPropertyChanged(nameof(this.Patronimic));
+            }
+        }
+        [DataMember]
+        [Required]
+        public string Login
+        {
+            get { return login; }
+            set
+            {
+                login = value;
+                OnPropertyChanged(nameof(this.Login));
+            }
+        }
+        [DataMember]
+        [Required]
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged(nameof(this.Password));
+            }
+        }
+        [DataMember]
+        [Required]
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                phone = value;
+                OnPropertyChanged(nameof(this.Phone));
+            }
+        }
+        [DataMember]
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+                OnPropertyChanged(nameof(this.Address));
+            }
+        }
+        [DataMember]
+        [Required]
+        public UserType UserType
+        {
+            get { return userType; }
+            set
+            {
+                userType = value;
+                OnPropertyChanged(nameof(this.UserType));
+            }
+        }
+    }
+}
